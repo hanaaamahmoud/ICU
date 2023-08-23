3bada9f074e8288d1c3c6cf0e03235adfd62ec44
Hanaa Mahmoud <hnaaljnzwry@gmail.com>
2023-08-23 22:33:32 +0300

    second commit

--- main.c
+++ main.c
@@ -136,41 +136,7 @@
 //}
 
 
-/**************************** Watch Dog************************/
-//void WDT_voidEnable(void);
-//void WDT_voidDisable(void);
-//void WDT_voidSleep(u8 Copy_u8Value);
-//
-//
-//
-//int main()
-//{
-//	DIO_u8SetPinDir(DIO_PORTA,DIO_PIN7,DIO_PIN_OUT);
-//
-//	DIO_u8SetPinValue(DIO_PORTA,DIO_PIN7,DIO_HIGH);
-//
-//	_delay_ms(1000);
-//
-//	DIO_u8SetPinValue(DIO_PORTA,DIO_PIN7,DIO_LOW);
-//	WDT_voidEnable();
-//	WDT_voidSleep(6);
-//
-//}
-//
-//void WDT_voidSleep(u8 Copy_u8Value)
-//{
-//WDTCR&=0b11111000;
-//WDTCR|=Copy_u8Value;
-//}
-//void WDT_voidEnable(void)
-//{
-//	SET_BIT(WDTCR,WDE);
-//}
-//void WDT_voidDisable(void)
-//{
-//WDTCR|=0b00011000;
-//WDTCR=0;
-//}
+
 
 
 
